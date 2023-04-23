8b728586098d7d4a4bace462b31fa7d3d37072bb
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2023-04-23 10:29:27 -0300

    test: implementa testes no código legal pra que possamos entender o código e tenhamos exemplo de cada coisa que o código faz

--- refactoring/test/main.test.ts
+++ refactoring/test/main.test.ts
@@ -12,3 +12,68 @@ test('Deve calcular a tarifa de uma corrida em um dia normal', () => {
 });
 
 
+test('Deve calcular a tarifa de uma corrida de noite', () => {
+  const fare = calc([
+    {
+      dist: 10,
+      ds: new Date('2021-03-01T23:00:00'),
+    },
+  ])  
+
+  expect(fare).toBe(39)
+});
+
+test('Deve calcular a tarifa de uma corrida domingo de dia', () => {
+  const fare = calc([
+    {
+      dist: 10,
+      ds: new Date('2021-03-07T10:00:00'),
+    },
+  ])  
+
+  expect(fare).toBe(29)
+});
+
+test('Deve calcular a tarifa de uma corrida domingo de noite', () => {
+  const fare = calc([
+    {
+      dist: 10,
+      ds: new Date('2021-03-07T23:00:00'),
+    },
+  ])  
+
+  expect(fare).toBe(50)
+});
+
+test('Deve retornar -1 se a distância for inválida', () => {
+  const fare = calc([
+    {
+      dist: -10,
+      ds: new Date('2021-03-07T23:00:00'),
+    },
+  ])  
+
+  expect(fare).toBe(-1)
+});
+
+test('Deve retornar -2 se a data for inválida', () => {
+  const fare = calc([
+    {
+      dist: 10,
+      ds: new Date('Adsfdsf'),
+    },
+  ])  
+
+  expect(fare).toBe(-2)
+});
+
+test('Deve retornar 10 como tarifa mínima', () => {
+  const fare = calc([
+    {
+      dist: 2,
+      ds: new Date('2021-03-07T10:00:00'),
+    },
+  ])  
+
+  expect(fare).toBe(10)
+});
