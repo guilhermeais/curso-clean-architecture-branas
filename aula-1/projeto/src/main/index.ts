00d6eb7471222758fab7316b4922af55c8cddf40
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2023-04-24 22:46:08 -0300

    refator: separa o app do server

--- aula-1/projeto/src/main/index.ts
+++ aula-1/projeto/src/main/index.ts
@@ -1,25 +1,15 @@
-import { OrdersController } from "../application/controller/orders-controller";
-import { CreateOrder } from "../application/use-cases/create-order";
-import { InMemoryOrderRepository } from "../infra/data/in-memory-order-repository";
-import { ExpressRouterAdapter } from "./adapters/express-adapter";
+import expressApp from "./app";
 
 const port = Number(process.env.PORT) || 3000
 
-const expressRouterAdapter = new ExpressRouterAdapter()
 
-const orderRepository = new InMemoryOrderRepository()
-const createOrder = new CreateOrder(orderRepository)
-const ordersController = new OrdersController(createOrder)
-
-expressRouterAdapter.post('/orders', ordersController)
-
-expressRouterAdapter.listen(port, () => console.log('Server running at http://localhost:3000'));
+expressApp.listen(port, () => console.log('Server running at http://localhost:3000'));
 
 const closeEvents = ['SIGINT', 'SIGTERM', 'SIGQUIT']
 
 closeEvents.forEach((event) => {
   process.on(event, async () => {
-    await expressRouterAdapter.close()
+    await expressApp.close()
     process.exit(0)
   })
 })
