dcb8d1c0d5a1aca8aeef61dfd0f465104981960d
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2023-05-12 20:52:42 -0300

    refactor: refatora value-object cpf com a nova função de validar cpf

--- aula-2/projeto/requirements-2.md
+++ aula-2/projeto/requirements-2.md
@@ -1,12 +1,12 @@
 ## Testes
 
-1. Não deve aplicar cupom de desconto expirado
-2. Ao fazer um pedido, a quantidade de um item não pode ser negativa
-3. Ao fazer um pedido, o mesmo item não pode ser informado mais de uma vez
-4. Nenhuma dimensão do item pode ser negativa
-5. O peso do item não pode ser negativo
-6. Deve calcular o valor do frete com base nas dimensões (altura, largura e profundidade em cm) e o peso dos produtos (em kg)
-7. Deve retornar o preço mínimo de frete caso ele seja superior ao valor calculado
+- Não deve aplicar cupom de desconto expirado
+- Ao fazer um pedido, a quantidade de um item não pode ser negativa
+- Ao fazer um pedido, o mesmo item não pode ser informado mais de uma vez
+- Nenhuma dimensão do item pode ser negativa
+- O peso do item não pode ser negativo
+- Deve calcular o valor do frete com base nas dimensões (altura, largura e profundidade em cm) e o peso dos produtos (em kg)
+- Deve retornar o preço mínimo de frete caso ele seja superior ao valor calculado
 
 
 ## Considere
--- aula-2/projeto/src/domain/value-objects/cpf.ts
+++ aula-2/projeto/src/domain/value-objects/cpf.ts
@@ -15,56 +15,49 @@ export class CPF {
     return this._value
   }
 
-  private validate(cpf: string) {
-    if (cpf !== null) {
-      if (cpf !== undefined) {
-        if (cpf.length >= 11 || cpf.length <= 14) {
-          cpf = cpf
-            .replace('.', '')
-            .replace('.', '')
-            .replace('-', '')
-            .replace(' ', '')
 
-          if (!cpf.split('').every(c => c === cpf[0])) {
-            try {
-              let d1, d2
-              let dg1, dg2, rest
-              let digito
-              let nDigResult
-              d1 = d2 = 0
-              dg1 = dg2 = rest = 0
-
-              for (let nCount = 1; nCount < cpf.length - 1; nCount++) {
-                // if (isNaN(parseInt(str.substring(nCount -1, nCount)))) {
-                // 	return false;
-                // } else {
-
-                digito = parseInt(cpf.substring(nCount - 1, nCount))
-                d1 = d1 + (11 - nCount) * digito
-
-                d2 = d2 + (12 - nCount) * digito
-                // }
-              }
-
-              rest = d1 % 11
-
-              dg1 = rest < 2 ? (dg1 = 0) : 11 - rest
-              d2 += 2 * dg1
-              rest = d2 % 11
-              if (rest < 2) dg2 = 0
-              else dg2 = 11 - rest
-
-              let nDigVerific = cpf.substring(cpf.length - 2, cpf.length)
-              nDigResult = '' + dg1 + '' + dg2
-              return nDigVerific == nDigResult
-            } catch (e) {
-              console.error('Erro !' + e)
-
-              return false
-            }
-          } else return false
-        } else return false
-      }
-    } else return false
+  private isValidLength(cpf: string) {
+    return cpf.length >= 11 || cpf.length <= 14
   }
+  
+  private isAllDigitsEqual(cpf: string) {
+    return cpf.split('').every(c => c === cpf[0])
+  }
+  
+  private removeNonDigits(cpf: string) {
+    return cpf.replace(/\D/g, '')
+  }
+  
+  private calculateDigit(cpf: string, factor: number) {
+    let total = 0
+  
+    for (const digit of cpf) {
+      if (factor > 1) total += parseInt(digit, 10) * factor--
+    }
+  
+    const rest = total % 11
+  
+    return rest < 2 ? 0 : 11 - rest
+  }
+  
+  private  validate(cpf: string) {
+    if (!cpf) {
+      return false
+    }
+  
+    if (!this.isValidLength(cpf)) {
+      return false
+    }
+  
+    cpf = this.removeNonDigits(cpf)
+    if (this.isAllDigitsEqual(cpf)) {
+      return false
+    }
+    const digitOne = this.calculateDigit(cpf, 10)
+    const digitTwo = this.calculateDigit(cpf, 11)
+    const actualDigit = cpf.slice(9)
+    const calculatedDigits = `${digitOne}${digitTwo}`
+    return actualDigit == calculatedDigits
+  }
+  
 }
