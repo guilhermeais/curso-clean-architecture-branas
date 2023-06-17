1f04736c8977e8f3be2577056d292e4807e0da98
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2023-06-16 21:08:38 -0300

    aula05: remove lixos da aula anterior

--- aula-4/hexagonal-architecture/test/cpf.test.ts
+++ aula-4/hexagonal-architecture/test/cpf.test.ts
@@ -1,13 +1,31 @@
 import { CPF } from '../src/cpf'
 
-test('Deve criar um cpf válido', () => {
-  const validCPF = '042.759.630-08'
+test.each([
+  "042.759.630-08",
+  "587.099.304-00",
+  "746.971.314-01",
+  "74697131401",
+])('Deve validar um cpf válido: %s', (validCPF) => {
   const cpf = new CPF(validCPF)
 
   expect(cpf).toBeDefined()
 })
 
-test('Deve criar um cpf inválido', () => {
-  const invalidCPF = '042.759.630-26'
+test.each([
+  "142.759.630-34",
+  "142.759.630",
+  "142.759",
+  "111.111.111-11",
+  "222.222.222-22",
+  "333.333.333-33",
+])('Deve validar um cpf inválido: %s', (invalidCPF) => {
   expect(() => new CPF(invalidCPF)).toThrowError('Invalid CPF')
 })
+
+
+test.each(
+  ['', null, undefined, NaN]
+)('Deve validar se o CPF é vazio %s', (invalidCPF) => {
+  expect(() => new CPF(invalidCPF as string)).toThrowError('Invalid CPF')
+
+});
