e4bf8ab9156c2e8802e1eb67b3fd656c52f09221
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2023-06-16 21:42:56 -0300

    aula05: aplica o padrão abstract factory para construir os repositórios

--- aula-5/clean-architecture/src/app.ts
+++ aula-5/clean-architecture/src/app.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express'
 import { Checkout } from './checkout'
 import ProductRepositoryInMemory from './product-repository-in-memory'
 import { CouponRepositoryInMemory } from './coupon-repository-in-memory'
+import { InMemoryRepositoryFactory } from './in-memory-repository-factory'
 
 
 const app = express()
@@ -11,8 +12,7 @@ app.use(express.json())
 app.post('/checkout', async (req: Request, res: Response) => {
   try {
     const checkoutService = new Checkout(
-      new ProductRepositoryInMemory(),
-      new CouponRepositoryInMemory()
+      new InMemoryRepositoryFactory()
     );
 
     const checkout = await checkoutService.execute(req.body)
