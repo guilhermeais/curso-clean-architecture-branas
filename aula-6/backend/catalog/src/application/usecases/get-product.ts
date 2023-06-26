82ced7c639d3040ac71bbd8f6b1448c6556e22ad
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2023-06-26 13:04:33 -0300

    refactor: corrige retorno do caso de uso
    como o caso de uso é uma regra de negócio independe que orquestra as entidades que são regras de negócios independentes, achei melhor retornar um outro tipo de dados invés da propria entidade.

--- aula-6/backend/catalog/src/application/usecases/get-product.ts
+++ aula-6/backend/catalog/src/application/usecases/get-product.ts
@@ -1,24 +1,38 @@
-import { Product } from "../../domain/entities/product-entity";
-import ProductsRepository from "../protocols/repositories/products-repository";
-import RepositoryFactory from "../protocols/repositories/repository-factory";
-import { UseCase } from "./usecase";
+import { Product } from '../../domain/entities/product-entity'
+import ProductsRepository from '../protocols/repositories/products-repository'
+import RepositoryFactory from '../protocols/repositories/repository-factory'
+import { UseCase } from './usecase'
 
-export class GetProduct implements UseCase<string, Product | null> {
+export class GetProduct implements UseCase<string, GetProductOutput | null> {
   private readonly productRepo: ProductsRepository
   constructor(repositoryFactory: RepositoryFactory) {
     this.productRepo = repositoryFactory.createProductsRepository()
   }
   async execute(productId: string) {
-    /**Deve haver muito cuidado ao retornar a entidade diretamente do repositório
-     * Isso porque o caso de uso é um responsável por fazer uma operação especifica 
-     * e consequentemente, ter um retorno especifico.
-     * 
-     * A entidade, já é uma regra de negócio que pode ser compartilhada e aplicada em
-     * qualquer contexto.
-     * 
-     * Desse modo, se em algum momento precisarmos retornar coisas diferentes e de outros 
-     * repositórios, teríamos alguns problemas aqui.
-     */
-    return this.productRepo.getProduct(productId) 
+   
+    const  product =  await this.productRepo.getProduct(productId)
+
+    if (!product) {
+      return null
+    }
+
+    return {
+      ...product,
+      id: product.id.toString(),
+      volume: product.volume,
+      density: product.density
+    }
   }
+}
+
+export type GetProductOutput = {
+  id: string
+  description: string
+  price: number
+  width: number
+  height: number
+  length: number
+  weight: number
+  volume: number
+  density: number
 }
