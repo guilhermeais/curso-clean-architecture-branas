2da271c2a6013bd6c55f62d0f11f5d553063ceb6
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2023-07-16 15:52:33 -0300

    test: garante que simulate-freight esteja funcionando corretamente

--- aula-7/backend/freight/test/integration/simulate-freight.test.ts
+++ aula-7/backend/freight/test/integration/simulate-freight.test.ts
@@ -1,11 +1,36 @@
 import SimulateFreight from '../../src/application/usecases/simulate-freight'
+import ZipCode from '../../src/domain/entities/zip-code'
+import { InMemoryRepositoryFactory } from '../../src/infra/factory/in-memory-repository-factory'
 
 let simulateFreight: SimulateFreight
+let repositoryFactory: InMemoryRepositoryFactory
 
 beforeEach(() => {
-  simulateFreight = new SimulateFreight()
+  repositoryFactory = new InMemoryRepositoryFactory() 
+  repositoryFactory.zipCodeRepository.zipCodes.set('22030060', new ZipCode('22030060', -27.5945, -48.5477))
+  repositoryFactory.zipCodeRepository.zipCodes.set('88015600', new ZipCode('88015600', -22.9129, -43.2003))
+  simulateFreight = new SimulateFreight(repositoryFactory)
 })
-test('Deve simular o frete', async () => {
+test('Deve simular o frete sem o calculo da distância', async () => {
+  const input = {
+    items: [
+      {
+        volume: 0.03,
+        density: 100,
+        quantity: 1,
+      },
+    ],
+    from: '88015601',
+    to: '22030060',
+  }
+
+  const output = await simulateFreight.execute(input)
+
+  expect(output.freight).toEqual(30)
+})
+
+
+test('Deve simular o frete com o calculo da distância', async () => {
   const input = {
     items: [
       {
@@ -20,5 +45,5 @@ test('Deve simular o frete', async () => {
 
   const output = await simulateFreight.execute(input)
 
-  expect(output.freight).toEqual(30)
+  expect(output.freight).toEqual(22.446653340244893)
 })
