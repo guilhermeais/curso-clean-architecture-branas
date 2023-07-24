6c5d155c7b48d6bf4ca687065769fb9fa5574f8e
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2023-07-24 20:15:03 -0300

    feat(User): utiliza static factory method para construir e recuperar o usuário

--- aula-7/backend/auth/src/domain/entities/user.entity.ts
+++ aula-7/backend/auth/src/domain/entities/user.entity.ts
@@ -1,8 +1,38 @@
+import { pbkdf2Sync, randomBytes } from 'crypto'
 import Email from './email'
 
 export default class User {
   email: Email
-  constructor(email: string, readonly password: string, readonly salt: string) {
+  private constructor(
+    email: string,
+    readonly password: string,
+    readonly salt: string
+  ) {
     this.email = new Email(email)
+  }
+
+  /**
+   *
+   * @description static factory method
+   */
+  static create(email: string, password: string) {
+    const salt = randomBytes(20).toString('hex')
+    const hashedPassword = pbkdf2Sync(
+      password,
+      salt,
+      64,
+      100,
+      'sha512'
+    ).toString('hex')
+
+    return new User(email, hashedPassword, salt)
+  }
+
+  /**
+   *
+   * @description static factory method
+   */
+  static restore(email: string, hashedPassword: string, salt: string) {
+    return new User(email, hashedPassword, salt)
   }
 }
