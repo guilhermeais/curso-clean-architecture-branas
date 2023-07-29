4a98025641676ecf479833966c0e6e05a104cb40
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2023-07-29 14:38:14 -0300

    test(stock): garante que o stock calculator irá calcular a quanti no estoque corretamente

--- aula-8/backend/stock/test/e2e/api.test.ts
+++ aula-8/backend/stock/test/e2e/api.test.ts
@@ -1,18 +1,6 @@
 import supertest from 'supertest'
 import httpController from '../../src/app'
-import TokenGenerator from '../../src/domain/entities/token-generator'
-import User from '../../src/domain/entities/user.entity'
 const request = supertest(httpController.httpServer.nativeListener)
 describe('POST /verify', () => {
-  test('Deve verificar um token', async () => {
-    const mockedUser = User.create('test@mail.com', 'abc123', 'pbkdf2')
-    const token = new TokenGenerator('secret').sign(mockedUser)
-    const result = await request.post('/verify').send({
-      token,
-    })
-
-    expect(result.statusCode).toEqual(200)
-    expect(result.body.email).toEqual(mockedUser.email.value)
-    expect(result.body.isValid).toEqual(true)
-  })
+  test('should ', async () => {})
 })
---
+++ aula-8/backend/stock/test/unit/stock-calculator.spec.ts
@@ -0,0 +1,29 @@
+import StockCalculator from '../../src/domain/entities/stock-calculator'
+import StockEntry from '../../src/domain/entities/stock-entry'
+
+describe('StockCalculator', () => {
+  test('Deve calcular o estoque de um determinado produto', () => {
+    const stockEntries = [
+      new StockEntry(1, 'in', 2),
+      new StockEntry(1, 'out', 2),
+      new StockEntry(1, 'in', 1),
+    ]
+
+    const total = StockCalculator.calculate(stockEntries)
+
+    expect(total).toEqual(1)
+  })
+
+  test('Deve calcular o estoque negativo de um determinado produto', () => {
+    const stockEntries = [
+      new StockEntry(1, 'in', 2),
+      new StockEntry(1, 'out', 2),
+      new StockEntry(1, 'in', 1),
+      new StockEntry(1, 'out', 3),
+    ]
+
+    const total = StockCalculator.calculate(stockEntries)
+
+    expect(total).toEqual(-2)
+  })
+})
