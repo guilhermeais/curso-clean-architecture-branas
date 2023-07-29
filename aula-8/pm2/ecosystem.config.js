d60b95033c8faf266f1c1d6fbd3674a0c4d2af21
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2023-07-29 14:24:16 -0300

    feat: usa pm2 para gerenciar os microserviços

---
+++ aula-8/pm2/ecosystem.config.js
@@ -0,0 +1,23 @@
+/**
+ * @type {import('pm2').StartOptions}
+ */
+module.exports = {
+  apps: [
+    {
+      name: 'checkout',
+      script: 'cd ../backend/checkout && npm start',
+    },
+    {
+      name: 'catalog',
+      script: 'cd ../backend/catalog && npm start',
+    },
+    {
+      name: 'auth',
+      script: 'cd ../backend/auth && npm start',
+    },
+    {
+      name: 'freight',
+      script: 'cd ../backend/freight && npm start',
+    },
+  ],
+}
